2215c2f3c93b2d577833f618ef36834790c9c669
guiruffo <60902803+guiruffo@users.noreply.github.com>
2020-06-05 08:22:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,25 @@
 const express = require('express');
+const Pool = require('pg').Pool;
+const cors = require('cors');
+
+const pool = new Pool({
+    user: 'fvpmjbfbkkohok',
+    password: '18cf974044dcc2d0eba982628b7c0894865430916d4b7c05e0f918e66715230d',
+    host: 'ec2-54-161-208-31.compute-1.amazonaws.com',
+    database: 'd2pub2rn6eak0j',
+    port: 5432,
+    ssl: {rejectUnauthorized: false}
+});
+
 const server = express();
-var tenis = [
-    {id: 1, marca: 'nike', preco: 450.50, numero: 42},
-    {id: 2, marca: 'adidas', preco: 350.90, numero: 44},
-    {id: 3, marca: 'all star', preco: 50.00, numero: 40},
-];
+
+server.use(cors());
 
 server.use(express.json());
-server.get('/modelos', function(request, response){
-    return response.json(tenis);
+
+
+server.get('/modelos', async function(request, response){
+    const result = await pool.query ('SELECT * FROM modelo');
 })
 
-server.get('/modelos/:id', function(request, response){
-    const id = request.params.id;
-    const modelos = tenis.filter(m => m.id == id);
-    return response.json(modelos);
-})
-
-server.post('/modelos', function(request, response){
-    const modelos = request.body;
-    tenis.push(modelos);
-    return response.status(201).send();
-})
-
-server.delete('/modelos/:id', function(request, response){
-    const id = request.params.id;
-    tenis = tenis.filter(m => m.id != id);
-    return response.status(200).send();
-})
-server.put('/modelos/:id', function(request, response){
-    const id = request.params.id;
-    const modelos = request.body;
-
-    tenis.forEach(m =>{
-        if(m.id == id) {
-            m.marca = modelos.marca;
-            m.preco = modelos.preco;
-            m.numero = modelos.numero;
-            return;
-        }
-    })
-    return response.send();
-})
 server.listen(process.env.PORT || 3000);
